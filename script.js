e03178352fd35509e7aaea123c590aeef7e022ee
maxbernard3 <max.bernard_@hotmail.com>
2024-05-15 12:24:37 +0200

    added local storage and declutering of the UI

--- script.js
+++ script.js
@@ -2,34 +2,26 @@ pawn1 = document.getElementById("pawn1");
 map = document.getElementById("map");
 
 pawn = pawn1;
-mile = 220/200;
-movSpeed = 40*mile;
+mile = 220 / 200;
+movSpeed = 40 * mile;
 
 bypass = true;
 
 function movPawn(event) {
-    pawnLeft = pawn.getBoundingClientRect().left + map.scrollLeft;
-    pawnTop = pawn.getBoundingClientRect().top + map.scrollTop;
-
     xClick = event.clientX + map.scrollLeft;
     yClick = event.clientY + map.scrollTop;
 
-    pawn.style.left = `${xClick}px`;
-    pawn.style.top = `${yClick}px`;
+    localStorage.setItem(`pawn_x`, xClick - 15);
+    localStorage.setItem(`pawn_y`, yClick - 15);
 
-    let data = "";
+    pawn.style.left = `${localStorage.getItem(`pawn_x`)}px`;
+    pawn.style.top = `${localStorage.getItem(`pawn_y`)}px`;
+}
 
-    /*x = (pawnLeft - xClick);
-    y = (pawnTop - yClick);
 
-    distances = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+window.addEventListener("load", function() {
+    pawn.style.left = `${localStorage.getItem(`pawn_x`)}px`;
+    pawn.style.top = `${localStorage.getItem(`pawn_y`)}px`;
+});
 
-    console.log(movSpeed - distances);
-
-    if(distances < movSpeed || bypass == true){
-      pawn.style.left = `${xClick}px`;
-      pawn.style.top = `${yClick}px`;
-    }*/
-  }
-  
 map.addEventListener("click", movPawn);
--- scriptTactical.js
+++ scriptTactical.js
@@ -6,6 +6,10 @@ pawn5 = document.getElementById("pawn5");
 pawn6 = document.getElementById("pawn6");
 pawn7 = document.getElementById("pawn7");
 pawn8 = document.getElementById("pawn8");
+pawn9 = document.getElementById("pawn9");
+pawn10 = document.getElementById("pawn10");
+pawn11 = document.getElementById("pawn11");
+pawn12 = document.getElementById("pawn12");
 
 btn1 = document.getElementById("btn1");
 btn2 = document.getElementById("btn2");
@@ -15,11 +19,15 @@ btn5 = document.getElementById("btn5");
 btn6 = document.getElementById("btn6");
 btn7 = document.getElementById("btn7");
 btn8 = document.getElementById("btn8");
+btn9 = document.getElementById("btn9");
+btn10 = document.getElementById("btn10");
+btn11 = document.getElementById("btn11");
+btn12 = document.getElementById("btn12");
 
 map = document.getElementById("map");
 
-const pawnArr = [pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8];
-const btnArr = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8];
+const pawnArr = [pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8, pawn9, pawn10, pawn11, pawn12];
+const btnArr = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12];
 
 pawn = pawnArr[0];
 
@@ -29,8 +37,11 @@ function movPawn(event) {
   xClick = event.clientX + map.scrollLeft;
   yClick = event.clientY + map.scrollTop;
 
-  pawn.style.left = `${xClick}px`;
-  pawn.style.top = `${yClick}px`;
+  localStorage.setItem(`${pawn.id}_x`, xClick - 15);
+  localStorage.setItem(`${pawn.id}_y`, yClick - 15);
+
+  pawn.style.left = `${localStorage.getItem(`${pawn.id}_x`)}px`;
+  pawn.style.top = `${localStorage.getItem(`${pawn.id}_y`)}px`;
 }
 
 function movActive(i){
@@ -43,6 +54,27 @@ function movActive(i){
   btnArr[i].style.borderStyle = "dashed";
 }
 
+function movSetPos(i){
+    pawn = pawnArr[i];
+    pawn.style.left = `${localStorage.getItem(`${pawn.id}_x`)}px`;
+    pawn.style.top = `${localStorage.getItem(`${pawn.id}_y`)}px`;
+
+    if(btnArr[i].children[0].src == `http://127.0.0.1:5500/pawn/`){
+        btnArr[i].style.display = "none";
+        pawnArr[i].style.display = "none";
+        localStorage.setItem(`${pawn.id}_x`, 0);
+        localStorage.setItem(`${pawn.id}_y`, 0);
+    };
+}
+
+window.addEventListener("load", function() {
+    for(i = 0; i < 12; i++){
+        movSetPos(i);
+
+    }
+    pawn = pawnArr[0];
+});
+
 btn1.addEventListener("click", (event)=>{movActive(0)});
 btn2.addEventListener("click", (event)=>{movActive(1)});
 btn3.addEventListener("click", (event)=>{movActive(2)});
@@ -51,6 +83,9 @@ btn5.addEventListener("click", (event)=>{movActive(4)});
 btn6.addEventListener("click", (event)=>{movActive(5)});
 btn7.addEventListener("click", (event)=>{movActive(6)});
 btn8.addEventListener("click", (event)=>{movActive(7)});
-
+btn9.addEventListener("click", (event)=>{movActive(8)});
+btn10.addEventListener("click", (event)=>{movActive(9)});
+btn11.addEventListener("click", (event)=>{movActive(10)});
+btn12.addEventListener("click", (event)=>{movActive(11)});
 
 map.addEventListener("click", movPawn);
